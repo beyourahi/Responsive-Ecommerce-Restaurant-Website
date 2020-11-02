3720edb4bff31d0b0cb868263885566de03dd7d8
Rahi Khan <65137116+rahikhan360@users.noreply.github.com>
2020-11-02 22:21:06 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+    return (
+        <Container>
+            <h1>Sup Bitches</h1>
+        </Container>
+    );
+};
+
+export default App;
